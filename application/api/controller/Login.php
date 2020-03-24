5fb6324f3938948cca3bebfdc75e45af63c2f84b
EilenC <chhccht2136@qq.com>
2020-03-24 16:27:29 +0800

    Init Project

---
+++ application/api/controller/Login.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\api\controller;
+/*
+ * @Author: Eilen 
+ * @Date: 2020-03-24 11:24:53 
+ * @Last Modified by: Eilen
+ * @Last Modified time: 2020-03-24 11:43:59
+ */
+
+class Login
+{
+    public function index()
+    {
+        echo 'controller:   Login ------------ function: index';
+        echo '<br/>';
+    }
+}
